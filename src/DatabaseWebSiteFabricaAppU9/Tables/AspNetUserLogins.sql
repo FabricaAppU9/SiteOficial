1052b1c23dc78319242c7f20b840536ff8dcd483
gabrielbaptista <gabriel.baptista@uni9.pro.br>
2016-10-01 10:00:26 -0300

    Removido

--- src/DatabaseWebSiteFabricaAppU9/Tables/AspNetUserLogins.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[AspNetUserLogins] (
-    [LoginProvider]       NVARCHAR (450) NOT NULL,
-    [ProviderKey]         NVARCHAR (450) NOT NULL,
-    [ProviderDisplayName] NVARCHAR (MAX) NULL,
-    [UserId]              NVARCHAR (450) NULL,
-    CONSTRAINT [PK_IdentityUserLogin<string>] PRIMARY KEY CLUSTERED ([LoginProvider] ASC, [ProviderKey] ASC),
-    CONSTRAINT [FK_IdentityUserLogin<string>_ApplicationUser_UserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[AspNetUsers] ([Id])
-);
-
