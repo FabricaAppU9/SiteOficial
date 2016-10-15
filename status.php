7efcf234185a922cf7df3dbd89973a1c68ba51fb
ricardohenrique <ricardo.hb@outlook.com>
2016-10-15 11:00:24 -0300

    fix/change status icons and information

--- status.php
+++ status.php
@@ -4,10 +4,11 @@
 	        <div class="col-lg-3 col-sm-3">
 	            <div style="visibility: hidden; -webkit-animation-name: none; -moz-animation-name: none; animation-name: none;-webkit-animation-duration: 1.5s; -moz-animation-duration: 1.5s; animation-duration: 1.5s;-webkit-animation-delay: 0.15s; -moz-animation-delay: 0.15s; animation-delay: 0.15s;" class="stat wow fadeInUp animated" data-wow-offset="30" data-wow-duration="1.5s" data-wow-delay="0.15s">
 	                <div class="icon-top red-text">
-	                    <i class="icon-design-graphic-tablet-streamline-tablet"></i>
+	                    <!-- <i class="icon-design-graphic-tablet-streamline-tablet"></i> -->
+	                	<i class="fa fa-star-o" aria-hidden="true"></i>
 	                </div>
 	                <div class="stat-text">
-		                <h3 class="white-text red-border-bottom">1</h3>
+		                <h3 class="white-text red-border-bottom">30</h3>
 		                <h6>Projetos completados</h6>
 	                </div>
 	            </div>
@@ -16,11 +17,12 @@
 	        <div class="col-lg-3 col-sm-3">
 	            <div style="visibility: hidden; -webkit-animation-name: none; -moz-animation-name: none; animation-name: none;-webkit-animation-duration: 1.75s; -moz-animation-duration: 1.75s; animation-duration: 1.75s;-webkit-animation-delay: 0.30s; -moz-animation-delay: 0.30s; animation-delay: 0.30s;" class="stat wow fadeInUp animated" data-wow-offset="30" data-wow-duration="1.75s" data-wow-delay="0.30s">
 	                <div class="icon-top green-text">
-	                    <i class="icon-man-people-streamline-user"></i>
+	                    <!-- <i class="icon-man-people-streamline-user"></i> -->
+	                    <i style="padding-left: 5px;" class="fa fa-coffee" aria-hidden="true"></i>
 	                </div>
 	                <div class="stat-text">
-	                	<h3 class="white-text green-border-bottom">1</h3>
-	                	<h6>Clientes felizes</h6>
+	                	<h3 class="white-text green-border-bottom">568</h3>
+	                	<h6>Cafés tomado</h6>
 	                </div>
 	            </div>
 	        </div>
@@ -28,10 +30,11 @@
 	        <div class="col-lg-3 col-sm-3">
 	            <div style="visibility: hidden; -webkit-animation-name: none; -moz-animation-name: none; animation-name: none;-webkit-animation-duration: 2s; -moz-animation-duration: 2s; animation-duration: 2s;-webkit-animation-delay: 0.45s; -moz-animation-delay: 0.45s; animation-delay: 0.45s;" class="stat wow fadeInUp animated" data-wow-offset="30" data-wow-duration="2s" data-wow-delay="0.45s">
 	                <div class="icon-top blue-text">
-	                    <i class="icon-email-mail-streamline"></i>
+	                    <!-- <i class="icon-email-mail-streamline"></i> -->
+	                    <i class="fa fa-envelope-o" aria-hidden="true"></i>
 	                </div>
 	                <div class="stat-text">
-	                	<h3 class="white-text blue-border-bottom">56,236</h3>
+	                	<h3 class="white-text blue-border-bottom">236</h3>
 	                	<h6>Emails</h6>
 	                </div>
 	            </div>
@@ -40,10 +43,11 @@
 	        <div class="col-lg-3 col-sm-3">
 	            <div style="visibility: hidden; -webkit-animation-name: none; -moz-animation-name: none; animation-name: none;-webkit-animation-duration: 2.25s; -moz-animation-duration: 2.25s; animation-duration: 2.25s;-webkit-animation-delay: 1s; -moz-animation-delay: 1s; animation-delay: 1s;" class="stat wow fadeInUp animated" data-wow-offset="30" data-wow-duration="2.25s" data-wow-delay="1s">
 	                <div class="icon-top yellow-text">
-	                    <i class="icon-picture-streamline-1"></i>
+	                    <!-- <i class="icon-picture-streamline-1"></i> -->
+	                    <i class="fa fa-camera" aria-hidden="true"></i>
 	                </div>
 	                <div class="stat-text">
-	                	<h3 class="white-text yellow-border-bottom">2,519</h3>
+	                	<h3 class="white-text yellow-border-bottom">519</h3>
 	                	<h6>Fotos tiradas</h6>
 	                </div>
 	            </div>
