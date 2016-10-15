3fe896cad979aa3926536d8d70c186f0de2817a1
ricardohenrique <ricardo.hb@outlook.com>
2016-10-15 10:24:30 -0300

    comeback contact form

--- contato.php
+++ contato.php
@@ -1,60 +1,44 @@
-<!-- =========================
-   CONTACT US         
-============================== -->
-
 <section class="contact-us" id="contact">
-<div class="container">
-	
-	<!-- SECTION HEADER -->
-        <div class="section-header">
+	<div class="container">
+	    <div class="section-header">
+			<h2 class="white-text">Quer fazer desta equipe?</h2>
+			<h6 class="white-text">
+				Você realmente gosta de tecnologia? Quer fazer parte desta incrível equipe? Entre em contato!
+			</h6>
+		</div>
 		
-		<!-- SECTION TITLE -->
-		<h2 class="white-text">Quer fazer desta equipe?</h2>
-		
-		<!-- SHORT DESCRIPTION ABOUT THE SECTION -->
-		<h6 class="white-text">
-			Você realmente gosta de tecnologia? Quer fazer parte desta incrível equipe? Entre em contato!
-		</h6>
+		<div class="row">
+			<form role="form" class="contact-form" id="contact-form">
+				<div style="visibility: hidden; -webkit-animation-name: none; -moz-animation-name: none; animation-name: none;-webkit-animation-duration: 1.5s; -moz-animation-duration: 1.5s; animation-duration: 1.5s;-webkit-animation-delay: 0.15s; -moz-animation-delay: 0.15s; animation-delay: 0.15s;" class="wow fadeInLeft animated" data-wow-offset="30" data-wow-duration="1.5s" data-wow-delay="0.15s">
+				<div class="col-lg-6 col-sm-6">
+					<input name="name" placeholder="Seu Nome" class="form-control input-box" id="name" type="text">
+				</div>
+				<div class="col-lg-6 col-sm-6">
+					<input name="email" placeholder="Seu Email" class="form-control input-box" id="email" type="email">
+				</div>
+				<div class="col-lg-4 col-sm-4">
+					<input name="ra" placeholder="Seu RA" class="form-control input-box" id="ra" type="number">
+				</div>
+				<div class="col-lg-4 col-sm-4">
+					<input name="campus" placeholder="Seu campus" class="form-control input-box" id="campus" type="campus">
+				</div>
+				<div class="col-lg-4 col-sm-4">
+					<input name="subject" placeholder="Curso" class="form-control input-box" id="subject" type="text">
+				</div>
+				</div>
+				
+				<div style="visibility: hidden; -webkit-animation-name: none; -moz-animation-name: none; animation-name: none;-webkit-animation-duration: 1.5s; -moz-animation-duration: 1.5s; animation-duration: 1.5s;-webkit-animation-delay: 0.15s; -moz-animation-delay: 0.15s; animation-delay: 0.15s;" class="col-md-12 wow fadeInRight animated" data-wow-offset="30" data-wow-duration="1.5s" data-wow-delay="0.15s">
+					<textarea name="message" class="form-control textarea-box" placeholder="Fale um pouco sobre você e sua experiência em tecnologia" id="message"></textarea>
+				</div>
+				<h4 class="success pull-left white-text">
+					Your message has been sent successfully.
+				</h4>
+				
+				<h4 class="error pull-left white-text">
+					E-mail must be valid and message must be longer than 1 character.
+				</h4>
+				<button style="visibility: hidden; -webkit-animation-name: none; -moz-animation-name: none; animation-name: none;-webkit-animation-duration: 1.5s; -moz-animation-duration: 1.5s; animation-duration: 1.5s;-webkit-animation-delay: 0.15s; -moz-animation-delay: 0.15s; animation-delay: 0.15s;" class="btn btn-primary custom-button red-btn wow fadeInLeft animated" id="submit" data-wow-offset="30" data-wow-duration="1.5s" data-wow-delay="0.15s" type="submit">Enviar Contato</button>
+			</form>
+		</div>
 	</div>
-	<!-- / END SECTION HEADER -->
-	
-	<!-- CONTACT FORM-->
-	<!--
-	<div class="row">
-		<form role="form" class="contact-form" id="contact-form">
-			<div style="visibility: hidden; -webkit-animation-name: none; -moz-animation-name: none; animation-name: none;-webkit-animation-duration: 1.5s; -moz-animation-duration: 1.5s; animation-duration: 1.5s;-webkit-animation-delay: 0.15s; -moz-animation-delay: 0.15s; animation-delay: 0.15s;" class="wow fadeInLeft animated" data-wow-offset="30" data-wow-duration="1.5s" data-wow-delay="0.15s">
-			<div class="col-lg-4 col-sm-4">
-				<input name="name" placeholder="Seu Nome" class="form-control input-box" id="name" type="text">
-			</div>
-			<div class="col-lg-4 col-sm-4">
-				<input name="email" placeholder="Seu Email" class="form-control input-box" id="email" type="email">
-			</div>
-			<div class="col-lg-4 col-sm-4">
-				<input name="ra" placeholder="Seu RA" class="form-control input-box" id="ra" type="number">
-			</div>
-			<div class="col-lg-4 col-sm-4">
-				<input name="campus" placeholder="Seu campus" class="form-control input-box" id="campus" type="campus">
-			</div>
-			<div class="col-lg-4 col-sm-4">
-				<input name="subject" placeholder="Curso" class="form-control input-box" id="subject" type="text">
-			</div>
-			</div>
-			
-			<div style="visibility: hidden; -webkit-animation-name: none; -moz-animation-name: none; animation-name: none;-webkit-animation-duration: 1.5s; -moz-animation-duration: 1.5s; animation-duration: 1.5s;-webkit-animation-delay: 0.15s; -moz-animation-delay: 0.15s; animation-delay: 0.15s;" class="col-md-12 wow fadeInRight animated" data-wow-offset="30" data-wow-duration="1.5s" data-wow-delay="0.15s">
-				<textarea name="message" class="form-control textarea-box" placeholder="Fale um pouco sobre você e sua experiência em tecnologia" id="message"></textarea>
-			</div>
-			
-					<h4 class="success pull-left white-text">
-						Your message has been sent successfully.
-					</h4>
-					
-					
-					<h4 class="error pull-left white-text">
-						E-mail must be valid and message must be longer than 1 character.
-					</h4>
-			<button style="visibility: hidden; -webkit-animation-name: none; -moz-animation-name: none; animation-name: none;-webkit-animation-duration: 1.5s; -moz-animation-duration: 1.5s; animation-duration: 1.5s;-webkit-animation-delay: 0.15s; -moz-animation-delay: 0.15s; animation-delay: 0.15s;" class="btn btn-primary custom-button red-btn wow fadeInLeft animated" id="submit" data-wow-offset="30" data-wow-duration="1.5s" data-wow-delay="0.15s" type="submit">Enviar Contato</button>
-		</form>
-	</div>
-	<!-- / END CONTACT FORM-->
-</div> <!-- / END CONTAINER -->
-</section> <!-- / END CONTACT US SECTION-->
+</section>
