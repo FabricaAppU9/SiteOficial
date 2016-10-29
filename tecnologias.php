ccf4d21807d277e99c996ce08262fb471bbd8fcd
ricardohenrique <ricardo.hb@outlook.com>
2016-10-29 09:23:48 -0200

    change layout tecnology page

--- tecnologias.php
+++ tecnologias.php
@@ -7,103 +7,131 @@
 			</h6>
 		</div>
 		
-		<div class="row">
-			<div style="visibility: hidden; -webkit-animation-name: none; -moz-animation-name: none; animation-name: none;-webkit-animation-duration: 1.5s; -moz-animation-duration: 1.5s; animation-duration: 1.5s;-webkit-animation-delay: 0.15s; -moz-animation-delay: 0.15s; animation-delay: 0.15s;" class="col-md-6 col-sm-6 wow fadeInLeft animated" data-wow-offset="30" data-wow-duration="1.5s" data-wow-delay="0.15s">
-				<div class="feature">
-					<div class="feature-icon">
-						<img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/android.png" alt="" width="50px">
+		<!-- <div class="row"> -->
+			<div style="visibility: hidden; -webkit-animation-name: none; -moz-animation-name: none; animation-name: none;-webkit-animation-duration: 1.5s; -moz-animation-duration: 1.5s; animation-duration: 1.5s;-webkit-animation-delay: 0.15s; -moz-animation-delay: 0.15s; animation-delay: 0.15s;" class="wow fadeInLeft animated" data-wow-offset="30" data-wow-duration="1.5s" data-wow-delay="0.15s">
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="feature ">
+							<div class="feature-icon">
+								<img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/android.png" alt="" width="50px">
+							</div>
+							<h5>Android Studio</h5>
+							<p>
+								Desenvolvimento nativo de aplicações para Android. 
+							</p>
+						</div>
 					</div>
-					<h5>Android Studio</h5>
-					<p>
-						Utilizamos o android studio para desenvolvimento nativo de aplicações para Android. 
-					</p>
+					
+					<div class="col-sm-4">
+						<div class="feature ">
+							<div class="feature-icon">
+								<img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/astah.png" alt="" width="50px">
+							</div>
+							<h5>Astah</h5>
+							<p>Utilizamos o astah para os diagramas da UML.</p>
+						</div>
+					</div>
+					
+					<div class="col-sm-4">
+						<div class="feature ">
+							<div class="feature-icon">
+								<img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/drive.png" alt="" width="50px">
+							</div>
+							<h5>Google Drive</h5>
+							<p>Armazenamento em nuvens.</p>
+						</div>
+					</div>
 				</div>
-				
-				<div class="feature">
-					<div class="feature-icon">
-						<img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/astah.png" alt="" width="50px">
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="feature ">
+							<div class="feature-icon">
+								<img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/git.jpg" alt="" width="50px">
+							</div>
+							<h5>Git</h5>
+							<p>Subir códigos para a aplicação.</p>
+						</div>
 					</div>
-					<h5>Astah</h5>
-					<p>Utilizamos o astah para os diagramas da UML.</p>
+					
+					<div class="col-sm-4">
+						<div class="feature ">
+							<div class="feature-icon">
+								<img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/github.png" alt="" width="50px">
+							</div>
+							<h5>Github</h5>
+							<p>Diretórios e divisão de tarefas entre a equipe.</p>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="feature ">
+							<div class="feature-icon">
+								<img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/visual_studio2.jpg" alt="" width="50px">
+							</div>
+							<h5>Visual Studio</h5>
+							<p>Utilizado para desenvolvimento em nuvem.</p>
+						</div>
+					</div>
 				</div>
-				
-				<div class="feature">
-					<div class="feature-icon">
-						<img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/drive.png" alt="" width="50px">
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="feature ">
+							<div class="feature-icon">
+								<img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/linkedin.png" alt="" width="50px">
+							</div>
+							<h5>Linkedin</h5>
+							<p>Rede social.</p>
+						</div>
 					</div>
-					<h5>Google Drive</h5>
-					<p>Compartilhamento de arquivos e armazenamento em nuvens.</p>
+					<div class="col-sm-4">
+						<div class="feature ">
+							<div class="feature-icon">
+								<img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/net.png" alt="" width="50px">
+							</div>
+							<h5>Netbeans 8.1</h5>
+							<p>Utilizamos a IDE para desenvolvimento Java.</p>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="feature ">
+							<div class="feature-icon">
+								<img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/skype.jpg" alt="" width="50px">
+							</div>
+							<h5>Skype</h5>
+							<p>Comunicação entre a equipe.</p>
+						</div>
+					</div>
 				</div>
-			
-				<div class="feature">
-					<div class="feature-icon">
-						<img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/git.jpg" alt="" width="50px">
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="feature ">
+							<div class="feature-icon">
+								<img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/slack.png" alt="" width="50px">
+							</div>
+							<h5>Slack</h5>
+							<p>Comunicação entre a equipe.</p>
+						</div>
 					</div>
-					<h5>Git</h5>
-					<p>Subir códigos para a aplicação.</p>
-				</div>
-				
-				<div class="feature">
-					<div class="feature-icon">
-						<img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/github.png" alt="" width="50px">
+					<div class="col-sm-4">
+						<div class="feature ">
+							<div class="feature-icon">
+								<img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/whatsapp.png" alt="" width="50px">
+							</div>
+							<h5>WhatsApp</h5>
+							<p>Comunicação entre a equipe.</p>
+						</div>
 					</div>
-					<h5>Github</h5>
-					<p>Diretórios com os códigos, divisão de tarefas entre a equipe.</p>
-				</div>
-				<div class="feature">
-					<div class="feature-icon">
-						<img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/visual_studio2.jpg" alt="" width="50px">
+					<div class="col-sm-4">
+						<div class="feature ">
+							<div class="feature-icon">
+								<img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/azure.png" alt="" width="50px">
+							</div>
+							<h5>Azure</h5>
+							<p>Sistema operacional em nuvem.</p>
+						</div>
 					</div>
-					<h5>Visual Studio</h5>
-					<p>Utilizado para desenvolvimento em nuvem.</p>
-				</div>
-			</div>
-
-			<div style="visibility: hidden; -webkit-animation-name: none; -moz-animation-name: none; animation-name: none;-webkit-animation-duration: 1.5s; -moz-animation-duration: 1.5s; animation-duration: 1.5s;-webkit-animation-delay: 0.15s; -moz-animation-delay: 0.15s; animation-delay: 0.15s;" class="col-md-6 col-sm-6 wow fadeInRight animated" data-wow-offset="30" data-wow-duration="1.5s" data-wow-delay="0.15s">
-				<div class="feature">
-					<div class="feature-icon">
-						<img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/linkedin.png" alt="" width="50px">
-					</div>
-					<h5>Linkedin</h5>
-					<p>Rede social.</p>
-				</div>
-				<div class="feature">
-					<div class="feature-icon">
-						<img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/net.png" alt="" width="50px">
-					</div>
-					<h5>Netbeans 8.1</h5>
-					<p>Utilizamos a IDE para desenvolvimento Java.</p>
-				</div>
-				<div class="feature">
-					<div class="feature-icon">
-						<img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/skype.jpg" alt="" width="50px">
-					</div>
-					<h5>Skype</h5>
-					<p>Comunicação entre a equipe.</p>
-				</div>
-				<div class="feature">
-					<div class="feature-icon">
-						<img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/slack.png" alt="" width="50px">
-					</div>
-					<h5>Slack</h5>
-					<p>Comunicação entre a equipe.</p>
-				</div>
-				<div class="feature">
-					<div class="feature-icon">
-						<img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/whatsapp.png" alt="" width="50px">
-					</div>
-					<h5>WhatsApp</h5>
-					<p>Comunicação entre a equipe.</p>
-				</div>
-				<div class="feature">
-					<div class="feature-icon">
-						<img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/azure.png" alt="" width="50px">
-					</div>
-					<h5>Azure</h5>
-					<p>Sistema operacional em nuvem.</p>
 				</div>
 			</div>
 		</div>
-	</div>
+	<!-- </div> -->
 </section>
 
