053b4fd16ca1d104be0672266e5edb93863eb178
ricardohenrique <ricardo.hb@outlook.com>
2016-10-29 11:44:00 -0200

    add and fix team

--- nossaEquipe.php
+++ nossaEquipe.php
@@ -23,7 +23,7 @@
 						<ul>
 							<li><a target="_blank" href="https://www.facebook.com/profile.php?id=100000048922241&fref=ts"><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li>
 							<li><a target="_blank" href="https://www.linkedin.com/in/daniel-ferreira-de-barros-junior-65111622?authType=name&authToken=7Zgn&trk=Skyline_click_NBM&sl=NBM%3B506464293%3A1474121162270%3B0%3B41539810%3B"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-							<li><a target="_blank" href=""><i class="fa fa-github" aria-hidden="true"></i></a></li>
+							<li><a target="_blank" href="https://github.com/DanielGTI"><i class="fa fa-github" aria-hidden="true"></i></a></li>
 							<!--<li><a href=""><i class="fa fa-skype"></i></a></li>-->
 						</ul>
 					</div>
@@ -45,7 +45,7 @@
 						<ul>
 							<li><a target="_blank" href="https://www.facebook.com/masterets1000?fref=ts"><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li>
 							<li><a target="_blank" href="https://www.linkedin.com/in/ernesto-teodoro-silva-62868347?authType=name&authToken=i5w7&trk=Skyline_click_NBM&sl=NBM%3B506464293%3A1474121162270%3B2%3B51424504%3B"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-							<li><a target="_blank" href=""><i class="fa fa-github" aria-hidden="true"></i></a></li>
+							<!-- <li><a target="_blank" href=""><i class="fa fa-github" aria-hidden="true"></i></a></li> -->
 							<!--<li><a target="_blank" href=""><i class="fa fa-skype"></i></a></li>-->
 						</ul>
 					</div>
@@ -89,7 +89,7 @@
 						<ul>
 							<li><a target="_blank" href="https://www.facebook.com/jsoft.souza?fref=ts"><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li>
 							<li><a target="_blank" href="https://www.linkedin.com/in/jo%C3%A3o-carlos-silva-de-souza-67427b15?authType=name&authToken=Cdvi&trk=Skyline_click_NBM&sl=NBM%3B506464293%3A1474121162270%3B1%3B77916445%3B"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-							<li><a target="_blank" href=""><i class="fa fa-github" aria-hidden="true"></i></a></li>
+							<!-- <li><a target="_blank" href=""><i class="fa fa-github" aria-hidden="true"></i></a></li> -->
 							<!--<li><a target="_blank" href=""><i class="fa fa-skype"></i></a></li>-->
 						</ul>
 					</div>
@@ -110,7 +110,7 @@
 						<ul>
 							<li><a target="_blank" href="https://www.facebook.com/profile.php?id=100004935793311&fref=ts"><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li>
 							<li><a target="_blank" href="https://www.linkedin.com/in/alexandre-correa-alves-4aa5ab9b?trk=nav_responsive_tab_profile"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-							<li><a target="_blank" href=""><i class="fa fa-github" aria-hidden="true"></i></a></li>
+							<!-- <li><a target="_blank" href=""><i class="fa fa-github" aria-hidden="true"></i></a></li> -->
 							<!--<li><a target="_blank" href=""><i class="fa fa-skype"></i></a></li>-->
 						</ul>
 					</div>
@@ -152,8 +152,8 @@
 					<div class="social-icons">
 						<ul>
 							
-							<li><a target="_blank" href=""><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-							<li><a target="_blank" href=""><i class="fa fa-github" aria-hidden="true"></i></a></li>
+							<li><a target="_blank" href="https://www.linkedin.com/in/daniele-prado-cavalcante-rodrigues-31abaa119?trk=hp-identity-photo"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+							<li><a target="_blank" href="https://github.com/danielerodrigues"><i class="fa fa-github" aria-hidden="true"></i></a></li>
 							<!--<li><a target="_blank" href=""><i class="fa fa-skype"></i></a></li>-->
 						</ul>
 					</div>
@@ -249,7 +249,7 @@
 			</div>
 		
 		
-			<div class="col-lg-3 col-sm-3">
+			<!-- <div class="col-lg-3 col-sm-3">
 				<div class="team-member">
 					<figure class="profile-pic">
 						<img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/marcelamenezes.jpg" alt="">
@@ -264,12 +264,12 @@
 						<ul>
 							<li><a href="https://www.facebook.com/marcela.menezesmaciel?fref=ts"><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li>
 							<li><a href="https://br.linkedin.com/in/marcela-menezes-17a71964"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-							<li><a href=""><i class="fa fa-github" aria-hidden="true"></i></a></li>
-							<!--<li><a href=""><i class="fa fa-skype"></i></a></li>-->
+							 <li><a href=""><i class="fa fa-github" aria-hidden="true"></i></a></li>
+							<li><a href=""><i class="fa fa-skype"></i></a></li>
 						</ul>
 					</div>
 				</div>
-			</div>
+			</div> -->
 		 
 		
 			<div class="col-lg-3 col-sm-3">
@@ -287,7 +287,7 @@
 						<ul>
 							<li><a href="https://www.facebook.com/vanessa.pereira.1428?fref=ts"><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li>
 							<li><a href="https://br.linkedin.com/in/vanessa-pereira-94011353"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-							<li><a href=""><i class="fa fa-github" aria-hidden="true"></i></a></li>
+							<!-- <li><a href=""><i class="fa fa-github" aria-hidden="true"></i></a></li> -->
 							<!--<li><a href=""><i class="fa fa-skype"></i></a></li>-->
 						</ul>
 					</div>
@@ -306,15 +306,156 @@
 					</div>
 					<div class="social-icons">
 						<ul>
-							<li><a href=""><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li>
+							<!-- <li><a href=""><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li>
 							<li><a href=""><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-							<li><a href=""><i class="fa fa-github" aria-hidden="true"></i></a></li>
+							<li><a href=""><i class="fa fa-github" aria-hidden="true"></i></a></li> -->
 							<!--<li><a href=""><i class="fa fa-skype"></i></a></li>-->
 						</ul>
 					</div>
 				</div>
 			</div>
 			<div class="col-lg-3 col-sm-3">
+				<div class="team-member">
+					<figure class="profile-pic">
+						<img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/user.jpg" alt="">
+					</figure>
+					<div class="member-details">
+						<h5 class="dark-text blue-border-bottom">Matheus Pedro</h5>
+						<div class="position">
+							<p>Programador maio/2016 - atual</p>
+						</div>
+					</div>
+					<!-- <div class="social-icons">
+						<ul>
+							 <li><a href=""><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li>
+							<li><a href=""><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+							<li><a href=""><i class="fa fa-github" aria-hidden="true"></i></a></li>
+							<li><a href=""><i class="fa fa-skype"></i></a></li>
+						</ul>
+					</div> -->
+				</div>
+			</div>
+			<div class="col-lg-3 col-sm-3">
+				<div class="team-member">
+					<figure class="profile-pic">
+						<img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/user.jpg" alt="">
+					</figure>
+					<div class="member-details">
+						<h5 class="dark-text blue-border-bottom">Jonathan Souza</h5>
+						<div class="position">
+							<p>Programador maio/2016 - atual</p>
+						</div>
+					</div>
+					<div class="social-icons">
+						<ul>
+							 <!-- <li><a href=""><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li> -->
+							<li><a href="https://www.linkedin.com/in/jonathan-de-paulo-souza-3bb935125"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+							<li><a href="https://github.com/JonathanP157"><i class="fa fa-github" aria-hidden="true"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3 col-sm-3">
+				<div class="team-member">
+					<figure class="profile-pic">
+						<img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/user.jpg" alt="">
+					</figure>
+					<div class="member-details">
+						<h5 class="dark-text blue-border-bottom">Heraldo de Souza</h5>
+						<div class="position">
+							<p>Programador maio/2016 - atual</p>
+						</div>
+					</div>
+					<div class="social-icons">
+						<ul>
+							 <!-- <li><a href=""><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li> -->
+							<li><a href="https://www.linkedin.com/in/heraldo-souza-b90793130?trk=nav_responsive_tab_profile"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+							<li><a href="https://github.com/Heraldo26"><i class="fa fa-github" aria-hidden="true"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3 col-sm-3">
+				<div class="team-member">
+					<figure class="profile-pic">
+						<img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/user.jpg" alt="">
+					</figure>
+					<div class="member-details">
+						<h5 class="dark-text blue-border-bottom">João Henrique</h5>
+						<div class="position">
+							<p>Programador outubro/2016 - atual</p>
+						</div>
+					</div>
+					<div class="social-icons">
+						<ul>
+							 <!-- <li><a href=""><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li> 
+							<li><a href="https://www.linkedin.com/in/heraldo-souza-b90793130?trk=nav_responsive_tab_profile"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+							<li><a href="https://github.com/Heraldo26"><i class="fa fa-github" aria-hidden="true"></i></a></li>-->
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3 col-sm-3">
+				<div class="team-member">
+					<figure class="profile-pic">
+						<img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/user.jpg" alt="">
+					</figure>
+					<div class="member-details">
+						<h5 class="dark-text blue-border-bottom">Ivan Ramos</h5>
+						<div class="position">
+							<p>Iniciação Científica outubro/2016 - atual</p>
+						</div>
+					</div>
+					<div class="social-icons">
+						<ul>
+							 <!-- <li><a href=""><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li> 
+							<li><a href="https://www.linkedin.com/in/heraldo-souza-b90793130?trk=nav_responsive_tab_profile"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+							<li><a href="https://github.com/Heraldo26"><i class="fa fa-github" aria-hidden="true"></i></a></li>-->
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3 col-sm-3">
+				<div class="team-member">
+					<figure class="profile-pic">
+						<img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/user.jpg" alt="">
+					</figure>
+					<div class="member-details">
+						<h5 class="dark-text blue-border-bottom">Rodolfo Pinto</h5>
+						<div class="position">
+							<p>Iniciação Científica outubro/2016 - atual</p>
+						</div>
+					</div>
+					<div class="social-icons">
+						<ul>
+							 <!-- <li><a href=""><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li> 
+							<li><a href="https://www.linkedin.com/in/heraldo-souza-b90793130?trk=nav_responsive_tab_profile"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+							<li><a href="https://github.com/Heraldo26"><i class="fa fa-github" aria-hidden="true"></i></a></li>-->
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3 col-sm-3">
+				<div class="team-member">
+					<figure class="profile-pic">
+						<img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/user.jpg" alt="">
+					</figure>
+					<div class="member-details">
+						<h5 class="dark-text blue-border-bottom">Ricardo Mota</h5>
+						<div class="position">
+							<p>Programador outubro/2016 - atual</p>
+						</div>
+					</div>
+					<div class="social-icons">
+						<ul>
+							<!-- <li><a href=""><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li>  -->
+							<li><a href="https://br.linkedin.com/in/ricardo-henrique-mota-9a1033104"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+							<li><a href="https://github.com/ricardohenrique"><i class="fa fa-github" aria-hidden="true"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<!--<div class="col-lg-3 col-sm-3">
 				<div class="team-member">
 					<figure class="profile-pic">
 						<img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/user.jpg" alt="">
@@ -327,14 +468,14 @@
 					</div>
 					<div class="social-icons">
 						<ul>
-							<li><a href=""><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li>
+							 <li><a href=""><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li>
 							<li><a href=""><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
 							<li><a href=""><i class="fa fa-github" aria-hidden="true"></i></a></li>
-							<!--<li><a href=""><i class="fa fa-skype"></i></a></li>-->
+							<li><a href=""><i class="fa fa-skype"></i></a></li>
 						</ul>
 					</div>
 				</div>
-			</div>
+			</div>-->
 		</div> 
 	</div>
 </section>
