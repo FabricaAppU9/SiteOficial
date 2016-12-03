11d9fa0c0ca7009dd26039a33a29b3d683e1fbce
Guilherme Sant Ana Fujita <Guilherme Sant Ana Fujita>
2016-12-03 11:05:57 -0200

    Estrutura do modal feita. Falta o conteúdo detalhado

--- projetos.php
+++ projetos.php
@@ -13,8 +13,8 @@
 		<div class="col-md-12" id="portfolio-list">
 			<ul class="cbp-rfgrid">
 	        <li style="visibility: hidden; -webkit-animation-name: none; -moz-animation-name: none; animation-name: none;-webkit-animation-duration: 1.5s; -moz-animation-duration: 1.5s; animation-duration: 1.5s;-webkit-animation-delay: 0.15s; -moz-animation-delay: 0.15s; animation-delay: 0.15s;" class="wow fadeInLeft animated" data-wow-offset="30" data-wow-duration="1.5s" data-wow-delay="0.15s">
-	        <a href="http://demo.templateocean.com/wrapbootstrap/zerif-html/v1.3.1/project.html" class="more"><img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/main_activity.png" alt="project" width="130px">
-				<div class="project-info">
+	        <center><a data-toggle="modal" href="#myModalProcalc" id="procalc"><img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/logoProCalc.jpg" alt="project" width="100%" style="max-width: 215px;">
+				<!--<div class="project-info">
 					<div class="project-details">
 						<h5 class="white-text red-border-bottom">
 						Pro-calc </h5>
@@ -22,13 +22,13 @@
 							Android
 						</div>
 					</div>
-				</div>
-				</a>
+				</div>-->
+				</a></center>
 	        </li>
 	        
 	        <li style="visibility: hidden; -webkit-animation-name: none; -moz-animation-name: none; animation-name: none;-webkit-animation-duration: 1.5s; -moz-animation-duration: 1.5s; animation-duration: 1.5s;-webkit-animation-delay: 0.15s; -moz-animation-delay: 0.15s; animation-delay: 0.15s;" class="wow fadeInDown animated" data-wow-offset="30" data-wow-duration="1.5s" data-wow-delay="0.15s">
-	        <a href="http://demo.templateocean.com/wrapbootstrap/zerif-html/v1.3.1/prospiro.bmp" class="more"><img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/prospiro.bmp" alt="project">
-				<div class="project-info">
+	        <a data-toggle="modal" href="#myModalProspiro" id="procalc"><img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/prospiro.bmp" alt="project" style="border-radius: 30px;">
+				<!--<div class="project-info">
 					<div class="project-details">
 						<h5 class="white-text red-border-bottom">
 						Pro-Spiro </h5>
@@ -36,7 +36,7 @@
 							Android
 						</div>
 					</div>
-				</div>
+				</div>-->
 				</a>
 	        </li>
 
@@ -47,4 +47,64 @@
 	    <a id="back-button" class="red-btn" href="#"><i class="icon-fontawesome-webfont-27"></i> Go Back</a>
 	</div>
 </section> 
-
+<div class="modal fade" id="myModalProcalc" role="dialog">
+	<div class="modal-dialog" style="min-width:80%;">
+		<!-- Modal content-->
+      <div class="modal-content" style="padding: 20px;min-height: auto;">
+      	<div class="modal-header" style="min-height: 40px;">
+      		
+      		<button type="button" class="close" data-dismiss="modal">&times;</button>
+      		<h3>Pro-Calc</h3>
+      	</div>
+      	<div class="modal-body">
+      	<table>
+      		<tr>
+      			<td>Descrição</td>
+      			<td>ProCalc para Android. Calcula dados espirométricos.</td>
+      		</tr>
+      		<tr>
+      			<td>Tecnologias</td>
+      			<td>Android Studio, Astah, Java, Netbeans, Git e GitHub.</td>
+      		</tr>
+      		<tr>
+      			<td></td>
+      		</tr>
+      	</table>
+      	</div>
+      	<div class="modal-footer">
+      	</div>
+      </div>
+	</div>
+</div>
+<div class="modal fade" id="myModalProspiro" role="dialog">
+	<div class="modal-dialog" style="min-width:80%;">
+		<!-- Modal content-->
+      <div class="modal-content" style="padding: 20px;min-height: auto;">
+      	<div class="modal-header" style="min-height: 40px;">
+      		
+      		<button type="button" class="close" data-dismiss="modal">&times;</button>
+      		<h3>Pro-Spiro</h3>
+      	</div>
+      	<div class="modal-body">
+      	<table>
+      		<tr>
+      			<td>Descrição</td>
+      			<td>ProCalc para Android. Calcula dados espirométricos.</td>
+      		</tr>
+      		<tr>
+      			<td>Tecnologias</td>
+      			<td>Android Studio, Astah, Java, Netbeans, Git e GitHub.</td>
+      		</tr>
+      		<tr>
+      			<td></td>
+      		</tr>
+      	</table>
+      	</div>
+      	<div class="modal-footer">
+      	</div>
+      </div>
+	</div>
+</div>
+<script>
+	$("#myModalProcalc").modal()
+</script>
