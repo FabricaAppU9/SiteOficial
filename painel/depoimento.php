2d4690f46cc96c8145d95979df6687dac2d07def
GuilhermeFujita <fujitaguilherme@gmail.com>
2017-08-19 11:38:36 -0300

    Correção da página de depoimento

---
+++ painel/depoimento.php
@@ -0,0 +1,65 @@
+<?php 
+	// teste
+	require_once("header.php");
+	require_once("classe/DaoUsuario.php");
+	require_once("classe/DaoAluno.php");
+	require_once("classe/DaoCargo.php");
+	require_once("classe/DaoCampus.php");
+
+	$obj_usuario = new DaoUsuario();
+	$obj_aluno   = new DaoAluno();
+	$obj_cargo   = new DaoCargo();
+	$obj_campus  = new DaoCampus();
+
+	$usuario     = $obj_usuario->buscaUsuarioPorId($conexao, $_SESSION["id"]);
+	//$id_usuario  = (int)$_SESSION["id"];
+	$aluno       = $obj_aluno->buscaAlunoPorUsuario($conexao,$_SESSION["id"]);
+	$cargos      = $obj_cargo->listaCargos($conexao);
+	$campus      = $obj_campus->buscaCargos($conexao);
+?>
+<section id="conteudo-depoimentos">
+		<div class="container">
+			<h1><i class="fa fa-comment" aria-hidden="true"></i>&nbsp;&nbsp;Depoimento</h1>
+			<div class="border-dotted"></div>
+			<div class="row">
+				<div class="col-md-12 aviso">
+					<center><p>Caro Aluno(a), você só poderá ter um depoimento, mas pode editá-lo ou exclui-lo</p></center>
+				</div>
+			</div>
+			<div class="row">
+				<table class="table table-bordered">
+					<tr>
+						<th>Depoimento</th>
+						<th>Data de criação</th>
+						<th>Data de atualização</th>
+						<th>Ação</th>
+					</tr>
+					<tr>
+						<td>Teste</td>
+						<td>26/03/2017</td>
+						<td>-</td>
+						<td><i class="fa fa-pencil" style="color:yellow;" aria-hidden="true" data-toggle="modal" data-target="#modal-mensagem"></i><i class="fa fa-times" style="color:red;" aria-hidden="true"></i></td>
+					</tr>
+				</table>
+			</div>
+			<div class="modal fade" id="modal-mensagem">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal"><span>×</span></button>
+					<h4 class="modal-title">Título da mensagem</h4>
+				</div>
+				<div class="modal-body">
+					<p>Conteúdo da mensagem</p>
+				</div>
+				<div class="modal-footer">
+					<!--<button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>-->
+				</div>
+			</div>
+		</div>
+	</div>
+		</div>
+
+	</section>
+
+<?php require_once("footer.php");?>
