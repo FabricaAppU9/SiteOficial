e45f53e7df2a0e5dd77c5a208c49f7da2d8a9478
GuilhermeFujita <fujitaguilherme@gmail.com>
2017-09-30 11:11:56 -0300

    Finalização do arquivo de edição do depoimento do professor

--- painel/editar-depoimento-professor.php
+++ painel/editar-depoimento-professor.php
@@ -4,3 +4,19 @@
 	require_once("classe/DaoDepoimentoProfessor.php");
 	require_once("classe/DepoimentoProfessor.php");
 	require_once("classe/Login.php");
+
+	$obj_depoimento    = new DepoimentoProfessor();
+	$obj_daodepoimento = new DaoDepoimentoProfessor();
+
+
+
+	$obj_depoimento->setTexto(utf8_decode($_POST['depoimento_edit']));
+	$obj_depoimento->setDataUpdate(date('y/m/d'));
+
+	$depoimento = $obj_daodepoimento->alteraDepoimento($conexao, $obj_depoimento, $_SESSION["id"]);
+
+	if($depoimento){
+		echo "Sucesso!";
+	}else{
+		echo "Erro!";
+	}
