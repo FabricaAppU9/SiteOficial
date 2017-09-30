ce7ce3d2f8f1422daeab6a03e650ff71a57b8882
GuilhermeFujita <fujitaguilherme@gmail.com>
2017-09-30 09:57:55 -0300

    Elaboração da estrutura do arquivo de salvar depoimento do professor

--- painel/salvar-depoimento-professor.php
+++ painel/salvar-depoimento-professor.php
@@ -4,3 +4,18 @@
 	require_once("classe/DaoDepoimentoProfessor.php");
 	require_once("classe/DepoimentoProfessor.php");
 	require_once("classe/login.php");
+
+	$obj_depoimento    = new DepoimentoProfessor();
+	$obj_daodepoimento = new DaoDepoimentoProfessor();
+
+	$obj_depoimento->setTexto($_POST['depoimento']);
+	$obj_depoimento->setDataInclusao(date('y/m/d'));
+
+	$depoimento = obj_daodepoimento->inserirDepoimento($conexao, $obj_depoimento, $_SESSION["id"])
+
+	if($depoimento){
+		echo "Sucesso";
+	}
+	else{
+		echo "Erro";
+	}
