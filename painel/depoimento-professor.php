7dc4e019f460c1dfd52ec10f768f02c782f3188c
GuilhermeFujita <fujitaguilherme@gmail.com>
2017-10-06 18:22:04 -0300

    Exibindo o depoimento do professor que está salvo no bd

--- painel/depoimento-professor.php
+++ painel/depoimento-professor.php
@@ -43,9 +43,14 @@
 						<th>Ação</th>
 					</tr>
 					<tr>
-						<td>Teste</td>
-						<td>26/03/2017</td>
-						<td>-</td>
+						<td><?=utf8_encode($depoimento['pdp_texto'])?></td>
+						<td><?=date('d/m/Y',  strtotime($depoimento['pdp_data_inclusao']))?></td>
+						<?php if (date('d/m/Y',  strtotime($depoimento['pdp_data_update'])) == 0000-00-00) { ?>
+    						<td>-</td>
+						<?php } else { ?>
+   							 <td><?=date('d/m/Y',  strtotime($depoimento['pdp_data_update']))?></td>
+						<?php } ?>
+
 						<td><a class="excluir-depoimento" title="Excluir Depoimento"><i class='fa fa-times' style='color:red;' aria-hidden='true'></i></a>&nbsp;&nbsp;<a class="editardepoimento" title="Editar Depoimento"><i class="fa fa-pencil" style="color:yellow;" aria-hidden="true"></i></a></td>
 					</tr>
 				</table>
