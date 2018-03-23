5269090d69bf04915c5215e3af48ce77dffc3d79
GuilhermeFujita <fujitaguilherme@gmail.com>
2018-03-23 17:00:39 -0300

    Criação das classes do projeto e inclusão do header e footer no arquivo de projeto

---
+++ painel/classe/DaoProjeto.php
@@ -0,0 +1,1 @@
+DaoProjeto.php
---
+++ painel/projetos.php
@@ -0,0 +1,16 @@
+<?php
+
+	require_once("header.php");
+
+?>
+
+<section id="conteudo-projetos">
+	<div class="container">
+		<h1><i class="fa fa-users" aria-hidden="true"></i>&nbsp;&nbsp;Projetos</h1>
+		<div class="border-dotted"></div>
+	</div>
+</section>
+
+
+
+<?php require_once("footer.php");?>
