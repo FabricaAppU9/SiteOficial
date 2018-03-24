0a42568204bd5f4e0570667b9dd7bdc8271d2e28
rafael <faelferreira201511@gmail.com>
2018-03-24 10:57:19 -0300

    Esqueleto dos métodos no Dao do projeto,

--- painel/classe/DaoProjeto.php
+++ painel/classe/DaoProjeto.php
@@ -1,1 +1,23 @@
-DaoProjeto.php
+<?php
+
+	require_once("conecta.php");
+	require_once("Projeto.php");
+
+	class DaoProjeto{
+
+		public function listaProjeto(){
+
+		}
+
+		public function inserirProjeto(){
+
+		}
+
+		public function alterarProjeto(){
+
+		}
+
+		public function excluirProjeto(){
+			
+		}
+	}
