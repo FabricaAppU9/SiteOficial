e20c3ef8dab265b09bc53c15eed258c49740f15e
GuilhermeFujita <fujitaguilherme@gmail.com>
2018-03-28 16:21:50 -0300

    Esqueleto da tabela na tela de projetos

--- painel/projetos.php
+++ painel/projetos.php
@@ -1,16 +1,35 @@
-<?php
-
+<?php 
+	// teste
 	require_once("header.php");
-
+	
 ?>
+<section id="conteudo-depoimentos">
+		<div class="container">
+			<h1><i class="fa fa-comment" aria-hidden="true"></i>&nbsp;&nbsp;Depoimento</h1>
+			<div class="border-dotted"></div>
+			<div class="row">
+				<div class="col-md-12 aviso">
+					<center><p>Caro Aluno(a), você só poderá ter um depoimento, mas pode editá-lo ou exclui-lo</p></center>
+				</div>
+			</div>
+			<div class="row">
+				<table class="table table-bordered" id="table_projetos">
+					<tr>
+						<th>Depoimento</th>
+						<th>Data de criação</th>
+						<th>Data de atualização</th>
+						<th>Ação</th>
+					</tr>
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td><a class="excluir-depoimento" title="Excluir Depoimento"><i class='fa fa-times' style='color:red;' aria-hidden='true'></i></a>&nbsp;&nbsp;<a class="editardepoimento" title="Editar Depoimento"><i class="fa fa-pencil" style="color:yellow;" aria-hidden="true"></i></a></td>
+					</tr>
+				</table>
+			</div>
+		</div>
 
-<section id="conteudo-projetos">
-	<div class="container">
-		<h1><i class="fa fa-users" aria-hidden="true"></i>&nbsp;&nbsp;Projetos</h1>
-		<div class="border-dotted"></div>
-	</div>
-</section>
-
-
+	</section>
 
 <?php require_once("footer.php");?>
