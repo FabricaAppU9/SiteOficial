136a53c36fc23a3e7f34e3e73023315ff377dbc7
GuilhermeFujita <fujitaguilherme@gmail.com>
2018-03-28 16:34:01 -0300

    Correção do esqueleto da tabela, faltando arrumar css

--- painel/projetos.php
+++ painel/projetos.php
@@ -5,29 +5,30 @@
 ?>
 <section id="conteudo-depoimentos">
 		<div class="container">
-			<h1><i class="fa fa-comment" aria-hidden="true"></i>&nbsp;&nbsp;Depoimento</h1>
+			<h1><i class="fa fa-comment" aria-hidden="true"></i>&nbsp;&nbsp;Projetos</h1>
 			<div class="border-dotted"></div>
 			<div class="row">
-				<div class="col-md-12 aviso">
-					<center><p>Caro Aluno(a), você só poderá ter um depoimento, mas pode editá-lo ou exclui-lo</p></center>
-				</div>
-			</div>
-			<div class="row">
-				<table class="table table-bordered" id="table_projetos">
+
+
+				<table class="table table-bordered" id="table_depoimento">
 					<tr>
-						<th>Depoimento</th>
-						<th>Data de criação</th>
-						<th>Data de atualização</th>
+						<th>Nome</th>
+						<th>Descrição</th>
+						<th>Data de início</th>
+						<th>Data de término</th>
 						<th>Ação</th>
 					</tr>
 					<tr>
 						<td></td>
 						<td></td>
 						<td></td>
+   						<td></td>
+
 						<td><a class="excluir-depoimento" title="Excluir Depoimento"><i class='fa fa-times' style='color:red;' aria-hidden='true'></i></a>&nbsp;&nbsp;<a class="editardepoimento" title="Editar Depoimento"><i class="fa fa-pencil" style="color:yellow;" aria-hidden="true"></i></a></td>
 					</tr>
 				</table>
 			</div>
+
 		</div>
 
 	</section>
