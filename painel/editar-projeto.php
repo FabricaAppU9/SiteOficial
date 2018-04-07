f36dba17a1cc436dc8054abcd35e8363610017df
GuilhermeFujita <fujitaguilherme@gmail.com>
2018-04-07 11:56:54 -0300

    Pegando informacoes do form e setando no objeto

--- painel/editar-projeto.php
+++ painel/editar-projeto.php
@@ -8,7 +8,14 @@
 	$obj_projeto    = new Projeto();
 	$obj_daoprojeto = new DaoProjeto();
 
-	$obj_projeto->setNome(utf8_decode($_POST['projeto_edit']));
+
+	$obj_projeto->setId(utf8_decode($_POST['id']));
+	$obj_projeto->setNome(utf8_decode($_POST['nome']));
+	$obj_projeto->setDescricao(utf8_decode($_POST['descricao']));
+	$obj_projeto->setDataInicio(utf8_decode($_POST['dt_inicio']));
+	$obj_projeto->setDataFim(utf8_decode($_POST['dt_termino']));
+	$obj_projeto->setId(utf8_decode($_POST['tecnologias']));
+
 
 	$projeto = $obj_daoprojeto->alterarProjeto($conexao, $obj_projeto, $obj_projeto->getId());
 
