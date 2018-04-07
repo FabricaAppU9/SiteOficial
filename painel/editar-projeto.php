80127fbe5f9004f0417012a8c5e5927e1bb8a3ab
GuilhermeFujita <fujitaguilherme@gmail.com>
2018-04-07 16:29:20 -0300

    Testando devido a erros ao salvar no bd

--- painel/editar-projeto.php
+++ painel/editar-projeto.php
@@ -12,17 +12,14 @@
 	$obj_projeto->setNome($_POST['nome']);
 	$obj_projeto->setDescricao($_POST['descricao']);
 	$obj_projeto->setDataInicio($_POST['dt_inicio']);
-	if ($_POST['dt_termino'] != "0000-00-00") {
-		$obj_projeto->setDataFim($_POST['dt_termino']);
-	}
-	
+	$obj_projeto->setDataFim($_POST['dt_termino']);
 	$obj_projeto->setTecnologias($_POST['tecnologias']);
-
 
 	$projeto = $obj_daoprojeto->alterarProjeto($conexao, $obj_projeto, $obj_projeto->getId());
 
 
-	//var_dump($projeto);
+
+	var_dump($obj_projeto->getId());
 
 	/*if($projeto){
 		echo "Sucesso!";
