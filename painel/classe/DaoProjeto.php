a3269862219d463c6076f99fdca1b636b224e422
GuilhermeFujita <fujitaguilherme@gmail.com>
2018-04-08 09:55:10 -0300

    Alterando função de alterarProjeto no Dao para atuar sobre todos os campos da tabela

--- painel/classe/DaoProjeto.php
+++ painel/classe/DaoProjeto.php
@@ -24,7 +24,7 @@
 		}
 
 		public function alterarProjeto($conexao, Projeto $projeto, $prj_id){
-			$query = "UPDATE projeto SET prj_nome = '{$projeto->getNome()}' WHERE prj_id = {$prj_id}"; 
+			$query = "UPDATE projeto SET prj_nome = '{$projeto->getNome()}', prj_descricao = '{$projeto->getDescricao()}', prj_data_inicio = '{$projeto->getDataInicio()}', prj_data_fim = '{$projeto->getDataFim()}', prj_tecnologias = '{$projeto->getTecnologias()}' WHERE prj_id = {$prj_id}"; 
 			return mysqli_query($conexao,$query);
 		}
 
