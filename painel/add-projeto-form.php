d0ae135bcd59c9b6bf2ac6daac0ca879bdeaa0b1
GuilhermeFujita <fujitaguilherme@gmail.com>
2018-04-14 09:18:41 -0300

    Form para adicionar projetos

---
+++ painel/add-projeto-form.php
@@ -0,0 +1,39 @@
+<?php 
+	// teste
+	require_once("header.php");
+	require_once("classe/DaoProjeto.php");
+
+	$idProjeto = $_GET['id'];
+
+?>
+
+<section id="conteudo-depoimentos">
+		<div class="container">
+			<h1><i class="fa fa-comment" aria-hidden="true"></i>&nbsp;&nbsp;Editar Projeto</h1>
+			<div class="border-dotted"></div>
+			<!--<div class="edit-depoimento">-->
+						<form method="POST" id="form-edit-projeto">
+							<input type="hidden" name="id" value="<?=$idProjeto?>">
+							<label>Nome</label>
+							<input type="text" name="nome" class="form-control">
+							<label>Descrição</label>
+							<input type="text" name="descricao" class="form-control">
+							<label>Data de Inicio (aaaa-MM-dd)</label>
+							<input type="text" name="dt_inicio" class="form-control">
+							<label>Data de término (aaaa-MM-dd)</label>
+							<input type="text" name="dt_termino" class="form-control">
+							<label>Tecnologias</label>
+							<input type="text" name="tecnologias" class="form-control">
+							<div class="row">
+								<div class="col-md-12 botaosalvar">
+									<button input type="submit" class="btn btn-default salvar" id="add-projeto"><i class="fa fa-floppy-o" aria-hidden="true"></i>&nbsp;&nbsp;SALVAR</button>
+								</div>
+							</div>
+						</form>
+				
+			<!--</div> Fim edit projeto-->
+		</div>
+
+	</section>
+
+<?php require_once("footer.php");?>
