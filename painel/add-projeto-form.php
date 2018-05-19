3f6ffbcb352a544ac2eab30820c69a09e8a4d47d
GuilhermeFujita <fujitaguilherme@gmail.com>
2018-05-19 10:48:16 -0300

    IDs para validacao

--- painel/add-projeto-form.php
+++ painel/add-projeto-form.php
@@ -10,15 +10,15 @@
 			<!--<div class="edit-depoimento">-->
 						<form method="POST" id="form-add-projeto">
 							<label>Nome</label>
-							<input type="text" name="nome" class="form-control">
+                                                        <input id="projeto_nome" type="text" name="nome" class="form-control">
 							<label>Descrição</label>
-							<input type="text" name="descricao" class="form-control">
+							<input id="projeto_desc" type="text" name="descricao" class="form-control">
 							<label>Data de Inicio (aaaa-MM-dd)</label>
-							<input type="text" name="dt_inicio" class="form-control">
+                                                        <input id="projeto_inicio" type="text" name="dt_inicio" class="form-control">
 							<label>Data de término (aaaa-MM-dd)</label>
-							<input type="text" name="dt_termino" class="form-control">
+                                                        <input id="projeto_fim" type="text" name="dt_termino" class="form-control">
 							<label>Tecnologias</label>
-							<input type="text" name="tecnologias" class="form-control">
+                                                        <input id="projeto_tec" type="text" name="tecnologias" class="form-control">
 							<div class="row">
 								<div class="col-md-12 botaosalvar">
 									<button input type="submit" class="btn btn-default salvar" id="add-projeto"><i class="fa fa-floppy-o" aria-hidden="true"></i>&nbsp;&nbsp;SALVAR</button>
