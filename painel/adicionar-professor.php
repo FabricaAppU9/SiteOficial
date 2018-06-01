36a70cc951f71412bfdf22d0dca6498f1bb7d682
GuilhermeFujita <fujitaguilherme@gmail.com>
2018-06-01 11:05:51 -0300

    Pegando atributos enviados do formulario

--- painel/adicionar-professor.php
+++ painel/adicionar-professor.php
@@ -1,1 +1,14 @@
-Passei aqui
+<?php
+
+    require_once("classe/conecta.php");
+    require_once("classe/Professor.php");
+    require_once("classe/DaoProfessor.php");
+    require_once("classe/DaoUsuario.php");
+    require_once("classe/Login.php");
+    
+    $obj_professor =    new Professor();
+    $obj_daoprofessor = new DaoProfessor();
+    $obj_daousuario =   new DaoUsuario();
+    
+    $obj_professor->setNome($_POST['nome']);
+    $obj_professor->setEmail($_POST['email']);
