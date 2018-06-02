e7c8c8f6e5499d1225e3522a6c2c862f4697e953
GuilhermeFujita <fujitaguilherme@gmail.com>
2018-06-02 08:45:08 -0300

    Corrigindo referencia do link

--- painel/professores.php
+++ painel/professores.php
@@ -32,7 +32,7 @@
 					<?php foreach($professores as $prof) { ?>
 						<td><?= $prof['prf_nome'] ?></td>
 						<td>
-                                                    <a href="desabilitar-professor.php?id=<?=$proj['prj_id']?>" title="Desabilitar Professor"><i class='fa fa-times' style='color:red;' aria-hidden='true'></i></a>&nbsp;&nbsp;
+                                                    <a href="desabilitar-professor.php?id=<?=$prof['prf_id']?>" title="Desabilitar Professor"><i class='fa fa-times' style='color:red;' aria-hidden='true'></i></a>&nbsp;&nbsp;
 						</td>
 					</tr>
                                         <?php } ?>
