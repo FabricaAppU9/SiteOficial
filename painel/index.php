537a7cac11b948f4dcbf859770ef594ce14404a4
GuilhermeFujita <fujitaguilherme@gmail.com>
2018-08-30 22:17:46 -0300

    Exibindo mensagem de bem-vindo a tela do professor

--- painel/index.php
+++ painel/index.php
@@ -4,6 +4,10 @@
         if($usuario['pus_ptu_id'] == 1){
 			$aluno       = $obj_aluno->buscaAlunoPorUsuario($conexao, $_SESSION["id"]);
 			$nome = utf8_encode($aluno['pal_nome']);
+		}else{
+			$professor = $obj_professor->buscaProfessorPorUsuario($conexao,$_SESSION["id"]);
+			$foto = $professor['prf_foto'];
+			$nome = utf8_encode($professor['prf_nome']);
 		}
 ?>
 	<p style="margin-top: 100px;" ><center style="font-size:50px;">Seja Bem Vindo(a) <?=$nome?></center></p>
