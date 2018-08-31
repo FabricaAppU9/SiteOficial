b98c03f5f8f2dd6a4d9c82a0e7d78e0204b526fa
GuilhermeFujita <fujitaguilherme@gmail.com>
2018-08-30 22:16:31 -0300

    Exibindo mensagem de bem-vindo a tela do aluno

--- painel/index.php
+++ painel/index.php
@@ -1,7 +1,14 @@
 <?php 
 	// teste
 	require_once("header.php");
-        echo "Teste de index 04/07" ;
+        if($usuario['pus_ptu_id'] == 1){
+			$aluno       = $obj_aluno->buscaAlunoPorUsuario($conexao, $_SESSION["id"]);
+			$nome = utf8_encode($aluno['pal_nome']);
+		}
+?>
+	<p style="margin-top: 100px;" ><center style="font-size:50px;">Seja Bem Vindo(a) <?=$nome?></center></p>
+
+<?php 
 	require_once("footer.php");
 
 ?>
