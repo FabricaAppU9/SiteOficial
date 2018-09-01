6156ccf0c465d164bffc2933e81efd69c95d2050
GuilhermeFujita <fujitaguilherme@gmail.com>
2018-09-01 11:48:38 -0300

    Remoção de comentarios

--- painel/classe/DaoDepoimentoAluno.php
+++ painel/classe/DaoDepoimentoAluno.php
@@ -1,17 +1,15 @@
 <?php
 
-//Fazer DAODepoimento
 	require_once("conecta.php");
 	require_once("Aluno.php");
 	require_once("DepoimentoAluno.php");
 
 	class DaoDepoimentoAluno{
 
+		//Função de alterar depoimento
 		public function alteraDepoimento($conexao, DepoimentoAluno $depoimento, $pal_id){
 			$query = "UPDATE depoimento_aluno SET pda_texto = '{$depoimento->getTexto()}', pda_data_update = '{$depoimento->getDataUpdate()}' WHERE pda_pal_id = {$pal_id}"; 
 			return mysqli_query($conexao,$query);
-			//return $query;
-			//return $aluno->getFacebook();
 		}
 
 		//Função de selecionar depoimento
@@ -23,12 +21,13 @@
 			return $depoimento;
 		}
 
-
+		//Função de inserir depoimento
 		public function inserirDepoimento($conexao, DepoimentoAluno $depoimento, $pal_id){
 			$query = "INSERT INTO depoimento_aluno (pda_texto, pda_data_inicio, pda_pal_id) values('{$depoimento->getTexto()}', '{$depoimento->getDataInicio()}', {$pal_id})";
 			return mysqli_query($conexao,$query);
 		}
-
+		
+		//Função de excluir depoimento
 		public function excluirDepoimento($conexao, $pal_id){
 			$query = "DELETE FROM depoimento_aluno where pda_pal_id= {$pal_id}";
 			return mysqli_query($conexao, $query);
