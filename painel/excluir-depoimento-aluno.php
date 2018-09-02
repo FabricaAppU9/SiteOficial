88f4fc7e0433814608b42fec5ac1ba4ebb280bcc
GuilhermeFujita <fujitaguilherme@gmail.com>
2018-09-02 11:30:52 -0300

    Chamada da funcao para excluir depoimento do aluno

---
+++ painel/excluir-depoimento-aluno.php
@@ -0,0 +1,18 @@
+<?php
+
+    require_once("classe/conecta.php");
+	require_once("classe/DaoDepoimentoAluno.php");
+	require_once("classe/DepoimentoAluno.php");
+    require_once("classe/Login.php");
+    
+    $obj_depoimento    = new DepoimentoAluno();
+    $obj_daodepoimento = new DaoDepoimentoAluno();
+
+    $depoimento = $obj_daodepoimento->excluirDepoimento($conexao, $obj_depoimento, $_SESSION["id"]);
+
+    if($depoimento){
+        header("Location: depoimento.php");
+        die(); 
+	}else{
+		echo "Erro!";
+	}    
