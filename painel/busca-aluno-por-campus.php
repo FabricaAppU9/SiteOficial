facf24f77ed6ff4685bbc07b2f29ab52deca1c97
GuilhermeFujita <fujitaguilherme@gmail.com>
2018-09-09 11:52:21 -0300

    Finalizacao da tabela para editar e excluir

--- painel/busca-aluno-por-campus.php
+++ painel/busca-aluno-por-campus.php
@@ -9,23 +9,30 @@
 	$pal_pca_id = $_POST['opcao'];
 	$alunos = $obj_daoaluno->buscaAlunoPorCampus($conexao, $pal_pca_id);
 
-	$html = "";
-	foreach($alunos as $aluno){
-		$aluno_id = $aluno['pal_id'];
-		$name = "teste";
-		$html .= "<tr>";
-		$html .= "<td>".$aluno['pal_ra']."</td>";
-		$html .= "<td>".utf8_encode($aluno['pal_nome'])."</td>";
-		$html .= "<td>".utf8_encode($aluno['pca_nome'])."</td>";
-		$html .= "<td>".utf8_encode($aluno['pcu_nome'])."</td>";
-		$html .= "<td>".utf8_encode($aluno['pcr_nome'])."</td>";
-		$html .= "<td>".utf8_encode($aluno['ppe_nome'])."</td>";
-		$html .= "<td>".utf8_encode($aluno['pal_semestre'])."</td>";
-		$html .= "<td>".
-		'<a href = "excluir.php?id='.$aluno_id.'">'.$name.'</a>'.
-		'<a href = "editar-aluno-form.php?id='.$aluno_id.'">'.$name.'</a>'.
-		"</td>";
-		$html .= "</tr>";
-	}
+	if(isset($pal_pca_id)){
+		$html = "";
+		foreach($alunos as $aluno){
+			$aluno_id = $aluno['pal_id'];
+			$espaco = "  ";
+			$html .= "<tr>";
+			$html .= "<td>".$aluno['pal_ra']."</td>";
+			$html .= "<td>".utf8_encode($aluno['pal_nome'])."</td>";
+			$html .= "<td>".utf8_encode($aluno['pca_nome'])."</td>";
+			$html .= "<td>".utf8_encode($aluno['pcu_nome'])."</td>";
+			$html .= "<td>".utf8_encode($aluno['pcr_nome'])."</td>";
+			$html .= "<td>".utf8_encode($aluno['ppe_nome'])."</td>";
+			$html .= "<td>".utf8_encode($aluno['pal_semestre'])."</td>";
+			$html .= "<td>".
+			'<a href = "excluir.php?id='.$aluno_id.'">'.$espaco.
+			'<i class="fa fa-times" style="color:red;" aria-hidden="true"></i></a>'.
+			'<a href = "editar-aluno-form.php?pal_id='.$aluno_id.'">'.$espaco.
+			'<i class="fa fa-pencil" style="color:yellow;" aria-hidden="true"></i></a>'.
+			"</td>";
+			$html .= "</tr>";
+		}
 
-	echo $html;
+		echo $html;
+	}
+	else{
+		echo "<center><h1>Selecione Campus</h1></center>";		
+	}
