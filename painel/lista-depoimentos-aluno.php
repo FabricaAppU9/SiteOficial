acfc2e68dd9df0e9db3f56238c0fd223385eced2
maahrodrigues <maahrdg.z@gmail.com>
2018-09-13 20:44:30 -0300

    Apenas criado botão de editar e excluir, não estão funcionando

--- painel/lista-depoimentos-aluno.php
+++ painel/lista-depoimentos-aluno.php
@@ -60,7 +60,41 @@
    							<td><?=date('d/m/Y',  strtotime($depoimento['pda_data_update']))?></td>
 						<?php } ?>       
                             
-							<td>editar depoimento</td>
+						<td>
+                            <a  href="excluir-depoimento-aluno.php?id=<?=$aluno['pal_id']?>"  
+                               class="excluir-depoimento" title="Excluir Depoimento"><i class='fa fa-times' style='color:red;' aria-hidden='true'> <!-- erro na Exclusão de depoimentos -->
+                            </i></a>&nbsp;&nbsp;<a class="editardepoimento" title="Editar Depoimento"><i class="fa fa-pencil" style="color:yellow;" aria-hidden="true"></i></a></td>
+					</tr>
+				</table>
+			</div>
+			<div class="add-depoimento" style="display: none;">
+				<div class="row">
+					<div class="col-md-12 textarea">
+						<form method="post" id="form-add-depoimento">
+							<textarea id="textId" name="depoimento" class="form-control" placeholder="Depoimento aqui"></textarea>
+						</form>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12 botaosalvar">
+						<a class="btn btn-default salvar" id="salvar-depoimento"><i class="fa fa-floppy-o" aria-hidden="true"></i>&nbsp;&nbsp;SALVAR</a>
+					</div>
+				</div>
+			</div>
+			<div class="edit-depoimento">
+				<div class="row">
+					<div class="col-md-12 textarea">
+						<form method="post" id="form-edit-depoimento">
+							<textarea id="textId" name="depoimento_edit" class="form-control" placeholder="Depoimento aqui"><?=utf8_encode($depoimento['pda_texto'])?></textarea>
+						</form>
+					</div>
+				</div>
+				<!--Alterar a partir daqui-->
+				<div class="row">
+					<div class="col-md-12 botaosalvar">
+						<a class="btn btn-default salvar" id="salvar-edit-depoimento"><i class="fa fa-floppy-o" aria-hidden="true"></i>&nbsp;&nbsp;ALTERAR</a>
+					</div>
+				</div>
                             
                               
 						</tr>
