3415fff6dff395474c493933b6763473470856e7
GuilhermeFujita <fujitaguilherme@gmail.com>
2018-09-15 10:26:20 -0300

    Terminando correções na tabela

--- painel/lista-depoimentos-aluno.php
+++ painel/lista-depoimentos-aluno.php
@@ -60,7 +60,8 @@
 							<td>
                             <a  href="excluir-depoimento-aluno.php?id=<?=$aluno['pal_id']?>"  
                                class="excluir-depoimento" title="Excluir Depoimento"><i class='fa fa-times' style='color:red;' aria-hidden='true'> <!-- erro na Exclusão de depoimentos -->
-                            </i></a>&nbsp;&nbsp;<a class="editardepoimento" title="Editar Depoimento"><i class="fa fa-pencil" style="color:yellow;" aria-hidden="true"></i></a></td>       
+                            </i></a>&nbsp;&nbsp;
+							<a class="editardepoimento" title="Editar Depoimento"><i class="fa fa-pencil" style="color:yellow;" aria-hidden="true"></i></a></td>       
 						</tr>
 					<?php }?>
 				</table>
@@ -78,7 +79,21 @@
 						<a class="btn btn-default salvar" id="salvar-depoimento"><i class="fa fa-floppy-o" aria-hidden="true"></i>&nbsp;&nbsp;SALVAR</a>
 					</div>
 				</div>
-			</div> <!--Fim adicionar-->
+			</div> <!--Fim adicionar depoimento-->
+			<div class="edit-depoimento">
+				<div class="row">
+					<div class="col-md-12 textarea">
+						<form method="post" id="form-edit-depoimento">
+							<textarea id="textId" name="depoimento_edit" class="form-control" placeholder="Depoimento aqui"><?=utf8_encode($depoimento['pda_texto'])?></textarea>
+						</form>
+					</div>
+				</div>
+				<!--Botão para salvar-->
+				<div class="row">
+					<div class="col-md-12 botaosalvar">
+						<a class="btn btn-default salvar" id="salvar-edit-depoimento"><i class="fa fa-floppy-o" aria-hidden="true"></i>&nbsp;&nbsp;ALTERAR</a>
+					</div>
+				</div> <!--Fim edit depoimento-->
 		</div>
 
 	</section>
