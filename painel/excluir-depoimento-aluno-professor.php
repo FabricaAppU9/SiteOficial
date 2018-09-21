4c17d79f47655644f51cf1e6ccc3d450000a8a1b
GuilhermeFujita <fujitaguilherme@gmail.com>
2018-09-20 22:27:48 -0300

    Lógica para excluir depoimento do aluno

--- painel/excluir-depoimento-aluno-professor.php
+++ painel/excluir-depoimento-aluno-professor.php
@@ -1,4 +1,21 @@
 <?php
 
-    $id = $_GET['id'];
-    echo $id;
+    require_once("classe/conecta.php");
+    require_once("classe/DaoDepoimentoAluno.php");
+  	require_once("classe/DepoimentoAluno.php");
+    require_once("classe/Login.php");
+	
+	        
+        $obj_daodepoimento = new DaoDepoimentoAluno();
+        $obj_depoimento = new DepoimentoAluno();
+        
+        $id_aluno = $_GET['id'];
+        
+        $depoimento = $obj_daodepoimento->excluirDepoimento($conexao, $obj_depoimento, $id_aluno);
+
+    if($depoimento){
+        header("Location: lista-depoimentos-aluno.php");
+        die(); 
+	}else{
+		echo "Erro!";
+	}    
