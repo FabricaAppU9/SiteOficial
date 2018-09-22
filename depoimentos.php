a96421206b3431a0ac1106b58f3994ce7d2e2441
GuilhermeFujita <fujitaguilherme@gmail.com>
2018-09-22 10:09:53 -0300

    Exibindo depoimento dos professores no front end

--- depoimentos.php
+++ depoimentos.php
@@ -12,8 +12,8 @@
 	$obj_professor 		= new Professor();
 
 	$alunos = $obj_daoaluno->listatodososdepoimentos($conexao);
+	$professores = $obj_daoprofessor->listatodososdepoimentos($conexao);
 ?>
-
 
 <section class="testimonial" id="testimonials">
 	<div class="container">
@@ -30,7 +30,7 @@
 					<div>
 						<div style="width: 100%; left: 0px; display: block;" >
 							<div class="row">
-								<div class="col-md-4" style="background-color:#FFFFFF;border-radius:5px;padding:10px;">
+								<div class="col-md-6" style="background-color:#FFFFFF;border-radius:5px;padding:10px; margin-bottom:10px;">
 									<div class="feedback-box">
 										<div class="message">
 											 “Temos grande alegria e enorme prazer mantermos o contato e acompanhar a evolução do software. É um sonho de décadas, agora se concretizando graças ao empenho de tão brilhante grupo da Fábrica de App da Uninove, que aceitaram o desafio de algo tão diferente e tendo à frente o Prof. Gabriel.”
@@ -55,7 +55,7 @@
 								</div> <!--Fim depoimento professor Nacif-->
 
 								<!--Depoimento Heraldo-->
-								<div class="col-md-4" style="background-color:#FFFFFF;border-radius:5px;padding:10px;margin-left:15px; margin-bottom:15px;">
+								<div class="col-md-6" style="background-color:#FFFFFF;border-radius:5px;padding:10px; margin-left:10px; margin-bottom:15px;">
 									<div class="feedback-box">
 										<div class="message">
 											 “Tive uma entrevista na área de desenvolvimento, e comentei que faço parte do projeto que levantou mais o meu conhecimento, facilitando o processo de entrevista.
@@ -88,7 +88,7 @@ Estou super grato pela Fábrica e super grato pela equipe”
 				
 			</div> <!--Fim col-md-12-->
 	
-				<div class="col-md-12">
+			<div class="col-md-12">
 				<div style="display: block;">
 					<div>
 						<div style="width: 100%; left: 0px; display: block;">
@@ -116,14 +116,51 @@ Estou super grato pela Fábrica e super grato pela equipe”
 										</div>
 									</div>
 									
-								</div> <!--Fim Teste-->
+								</div>
 							</div> <!--Fim row-->
 							<?php } ?>
 						</div>
 					</div>
 				</div>
 				
-			</div> <!--col teste-->
+			</div> <!--col depoimento dos alunos-->
+
+						<div class="col-md-12">
+				<div style="display: block;">
+					<div>
+						<div style="width: 100%; left: 0px; display: block;">
+							<?php foreach($professores as $depoimento_professor){?>
+							<div class="row">
+								<div class="col-md-6" style="background-color:#FFFFFF;border-radius:5px;padding:10px;  margin-bottom: 10px;">
+									<div class="feedback-box">
+										<div class="message">
+											 “<?=$depoimento_professor['pdp_texto']?>”
+										</div>
+										
+										<div class="client">
+											<div class="quote red-text">
+												<img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/uninove.jpg" alt="" width="70px">
+											</div>
+											<div class="client-info">
+												<a class="client-name" href=""><?=$depoimento_professor['prf_nome']?></a> 
+												<div class="client-company">
+													 Professor Uninove
+												</div>
+											</div>
+											<div class="client-image hidden-xs">
+												<img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/uninove.jpg" alt="" width="70px" style="margin-top: 13px;">
+											</div>
+										</div>
+									</div>
+									
+								</div>
+							</div> <!--Fim row-->
+							<?php } ?>
+						</div>
+					</div>
+				</div>
+				
+			</div> <!--col depoimento professores-->
 		</div>
 	</div>
 </section>
