e67242d2e7104dabe2946217e47fb7030d9e1fb3
GuilhermeFujita <fujitaguilherme@gmail.com>
2018-09-22 11:43:27 -0300

    Correções para adicionar o projeto

--- painel/adicionar-projeto.php
+++ painel/adicionar-projeto.php
@@ -1,22 +1,34 @@
 <?php
 
+	error_reporting(0);
+
 	require_once("classe/conecta.php");
-	require_once ("classe/DaoProjeto.php");
+	require_once("classe/DaoProjeto.php");
 	require_once("classe/Projeto.php");
 	require_once("classe/Login.php");
 
 	$obj_projeto    = new Projeto();
 	$obj_daoprojeto = new DaoProjeto();
         
-        $data_inicio = $_POST['dt_inicio'];
-        $data_inicio = explode("/", $data_inicio);
+    $data_inicio = $_POST['dt_inicio'];
+    if(!isset($data_inicio) && empty($data_inicio)){
+		$data_inicio = 0000-00-00;	
+	}
+	else{
+		$data_inicio = explode("/", $data_inicio);
         list($dia, $mes, $ano) = $data_inicio;
         $data_inicio = $ano."-".$mes."-".$dia;
+	}
         
-        $data_fim = $_POST['dt_termino'];
-        $data_fim = explode("/", $data_fim);
-        list($dia, $mes, $ano) = $data_fim;
-        $data_fim = $ano."-".$mes."-".$dia;
+	$data_fim = $_POST['dt_termino'];
+	if(!isset($data_fim) && empty($data_fim)){
+		$data_fim = 0000-00-00;
+	}
+	else{
+		$data_fim = explode("/", $data_fim);
+		list($dia, $mes, $ano) = $data_fim;
+		$data_fim = $ano."-".$mes."-".$dia;
+	}
         
 	$obj_projeto->setNome($_POST['nome']);	
 	$obj_projeto->setDescricao($_POST['descricao']);
