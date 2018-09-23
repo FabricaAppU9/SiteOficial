88cc251d3c0b6ebd4ba6a9a55180546560cbf7d9
GuilhermeFujita <fujitaguilherme@gmail.com>
2018-09-23 16:08:47 -0300

    Remocao de comentarios e linhas desnecessarias no arquivo

--- painel/adicionar-projeto.php
+++ painel/adicionar-projeto.php
@@ -1,6 +1,6 @@
 <?php
 
-	//error_reporting(0);
+	error_reporting(0);
 
 	require_once("classe/conecta.php");
 	require_once("classe/DaoProjeto.php");
@@ -39,9 +39,6 @@
 
 	$projeto = $obj_daoprojeto->inserirProjeto($conexao, $obj_projeto, NULL);
 
-
-
-	//var_dump($obj_projeto);
 
 	if($projeto){
 		echo "Sucesso!";
