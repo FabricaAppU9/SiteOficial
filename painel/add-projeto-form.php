527de47b942b012fde883d8553954be529084319
GuilhermeFujita <fujitaguilherme@gmail.com>
2018-09-23 16:17:28 -0300

    Removendo campo desncessario

--- painel/add-projeto-form.php
+++ painel/add-projeto-form.php
@@ -14,11 +14,6 @@
 				<!--Form upload de foto-->
 				<div class="col-md-12">
 					<form method="POST" id="form-add-projeto">
-							<div class="col-md-12 img-perfil">
-								<label for="trocar-img"><i class="fa fa-camera" aria-hidden="true"></i>&nbsp;&nbsp;Trocar Imagem</label>
-								<input type="file" id="trocar-img" name="foto_perfil"><br>
-								<p id="arquivo-selecionado" style="color:green;">Nenhum arquivo selecionado!</p>
-							</div>
 							<label>Nome</label>
 							<input id="projeto_nome" type="text" name="nome" class="form-control">
 							<label>Descrição</label>
