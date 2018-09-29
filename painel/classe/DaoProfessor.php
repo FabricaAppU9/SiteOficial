d68d2e21d8f4912bec54d5c3782697401834a7d9
GuilhermeFujita <fujitaguilherme@gmail.com>
2018-09-29 16:20:55 -0300

    Funcao para listar depoimento dos professores

--- painel/classe/DaoProfessor.php
+++ painel/classe/DaoProfessor.php
@@ -19,34 +19,48 @@
 		}
 
 		public function alteraUsuario($conexao, Professor $professor, $data_update, $prf_id){
-			$query = "UPDATE professor SET prf_nome = '{$professor->getNome()}',  prf_facebook = '{$professor->getFacebook()}', prf_github = '{$professor->getGithub()}', prf_linkedin = '{$professor->getLinkedin()}', prf_data_update = '{$data_update}' WHERE prf_pus_id = {$prf_id}"; 
+			$query = "UPDATE professor SET prf_nome = '{$professor->getNome()}',  prf_facebook = '{$professor->getFacebook()}', prf_github = '{$professor->getGithub()}', prf_linkedin = '{$professor->getLinkedin()}', prf_data_update = '{$data_update}' WHERE prf_pus_id = {$prf_id}";
 			return mysqli_query($conexao,$query);
 			//return $query;
 			//return $aluno->getFacebook();
 		}
-                
+
                 public function listaProfessores($conexao){
                     $professores = array();
-                    
-                    $query = "SELECT * FROM professor INNER JOIN cargo 
+
+                    $query = "SELECT * FROM professor INNER JOIN cargo
                     ON professor.prf_pcr_id = cargo.pcr_id WHERE prf_habilitado = 1";
                     $resultado = mysqli_query($conexao, $query);
-                    
+
                     while ($professor = mysqli_fetch_assoc($resultado)){
                         array_push($professores, $professor);
                     }
-                    
+
                     return $professores;
                 }
-                
+
                 public function adicionaProfessor($conexao, Professor $professor){
                     $query = "INSERT INTO professor (prf_nome, prf_pus_id, prf_habilitado, prf_pcr_id) VALUES ('{$professor->getNome()}', '{$professor->getPus_id()}', 1 , 3) ";
                     return mysqli_query($conexao, $query);
-                    
+
                 }
-                
+
                 public function desabilitarProfessor($conexao, $prf_id){
                     $query = "UPDATE professor SET prf_habilitado = 0 WHERE prf_id = {$prf_id}";
                     return mysqli_query($conexao,$query);
                 }
+
+				public function listatodososdepoimentos($conexao){
+					$professores    = array();
+
+					$query     = "SELECT * FROM depoimento_professor left join professor on professor.prf_pus_id = depoimento_professor.pdp_prf_id";
+					$resultado = mysqli_query($conexao, $query);
+
+					while($professor = mysqli_fetch_assoc($resultado)){
+						array_push($professores,$professor);
+					}
+
+
+					return $professores;
+				}
 	}
