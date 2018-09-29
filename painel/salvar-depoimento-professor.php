48fcef92224e9b6057bd4f52e4718b279a89792a
GuilhermeFujita <fujitaguilherme@gmail.com>
2018-09-29 15:15:40 -0300

    Arquivo para salvar depoimento professor

--- painel/salvar-depoimento-professor.php
+++ painel/salvar-depoimento-professor.php
@@ -1,21 +1,21 @@
 <?php
 
-	require_once("classe/conecta.php");
-	require_once("classe/DaoDepoimentoProfessor.php");
-	require_once("classe/DepoimentoProfessor.php");
-	require_once("classe/login.php");
+	 require_once("classe/conecta.php");
+	 require_once("classe/DaoDepoimentoProfessor.php");
+	 require_once("classe/DepoimentoProfessor.php");
+	 require_once("classe/login.php");
 
-	$obj_depoimento    = new DepoimentoProfessor();
-	$obj_daodepoimento = new DaoDepoimentoProfessor();
+	 $obj_depoimento    = new DepoimentoProfessor();
+	 $obj_daodepoimento = new DaoDepoimentoProfessor();
 
-	$obj_depoimento->setTexto(utf8_decode($_POST['depoimento']));
-	$obj_depoimento->setDataInclusao(date('y/m/d'));
+	 $obj_depoimento->setTexto(utf8_decode($_POST['depoimento']));
+	 $obj_depoimento->setDataInclusao(date('y/m/d'));
 
-	$depoimento = $obj_daodepoimento->inserirDepoimento($conexao, $obj_depoimento, $_SESSION["id"]);
+	 $depoimento = $obj_daodepoimento->inserirDepoimento($conexao, $obj_depoimento, $_SESSION["id"]);
 
-	if($depoimento){
+	 if($depoimento){
 		echo "Sucesso";
-	}
-	else{
+	 }
+	 else{
 		echo "Erro";
-	}
+	 }
