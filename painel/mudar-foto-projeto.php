eae72a7bd563ba344a39e6513dd30efc9ced7892
GuilhermeFujita <fujitaguilherme@gmail.com>
2018-09-30 09:34:32 -0300

    Pegando id, e inserindo chamada da função para salvar no banco

--- painel/mudar-foto-projeto.php
+++ painel/mudar-foto-projeto.php
@@ -1,10 +1,11 @@
 <?php
-	// teste
+
 	require_once("classe/conecta.php");
 	require_once("classe/DaoProjeto.php");
 	require_once("classe/Login.php");
-	session_start();
-	ini_set('session.use_trans_sid', 10);
+
+	$idProjeto = $_POST['id_projeto'];
+
 	$obj_projeto = new DaoProjeto();
 
 	$arquivo_tmp = $_FILES[ 'foto_perfil' ][ 'tmp_name' ];
@@ -16,20 +17,18 @@
 
 	if ( strstr ( '.jpg;.jpeg;.gif;.png', $extensao ) ) {
 
-		$novoNome = uniqid ( time () ) . '_' . $_SESSION["id"] . '.' . $extensao;
+		$novoNome = uniqid ( time () ) . '_' . md5(time()) . '.' . $extensao;
 
 		$destino = 'imagens/alunos/' . $novoNome;
 
 		if ( @move_uploaded_file ( $arquivo_tmp, $destino ) ) {
 
-			$obj_projeto->MudaFoto($conexao,$novoNome,$_SESSION["id"]);
-			$_SESSION['foto'] = "Imagem alterada com Sucesso!";
-			header("Location: minhaconta.php");
+			$obj_projeto->MudaFoto($conexao,$novoNome,$idProjeto);
+			header("Location: projetos.php");
 
 			print_r("Sucesso!");
 		}else{
 			echo "Erro";
-			header("Location: minhaconta.php");
 		}
 	}else{
 		echo "Extensão não aceita!, por favor, informe um arquivo com .jpg, .jpeg, .gif ou .png";
