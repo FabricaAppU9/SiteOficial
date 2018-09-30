bbb1dcf01bca24314591d9973d1cf255ce17721a
GuilhermeFujita <fujitaguilherme@gmail.com>
2018-09-29 23:19:21 -0300

    Alteracoes no layout devido a funcionalidade de upload de foto

--- painel/editar-projeto-form.php
+++ painel/editar-projeto-form.php
@@ -1,5 +1,5 @@
-<?php 
-	// teste
+<?php
+
 	require_once("header.php");
 	require_once("classe/DaoProjeto.php");
 
@@ -8,13 +8,34 @@
 	$obj_projetos = new DaoProjeto();
 	$linhaProjeto = $obj_projetos->buscaProjetoPorId($conexao, $idProjeto);
 ?>
-
-<section id="conteudo-depoimentos">
+<section id="conteudo-minhaconta">
+	<div id="teste"></div>
 		<div class="container">
-			<h1><i class="fa fa-comment" aria-hidden="true"></i>&nbsp;&nbsp;Editar Projeto</h1>
+			<h1><i class="fa fa-user" aria-hidden="true"></i>&nbsp;&nbsp;Editar Projeto</h1>
 			<div class="border-dotted"></div>
-			<!--<div class="edit-depoimento">-->
-						<form method="POST" id="form-edit-projeto">
+			<?php if(isset($_SESSION['foto'])){?>
+				<p style="color:green;"><?=$_SESSION['foto']?></p>
+			<?php }?>
+			<div class="row">
+				<!--formulario responsavel pela fotografia-->
+				<form id="trocar-foto" method="post" enctype="multipart/form-data" action="mudar-foto.php">
+					<input type="hidden" name="id_usuario" value="<?=$_SESSION["id"]?>">
+					<div class="col-md-6 img-perfil">
+						<?php if($linhaProjeto['prj_foto'] == null){?>
+							<img src="imagens/alunos/usuario.png" class="img-circle" width="50%"><br>
+						<?php }else{?>
+							<img src="imagens/alunos/<?=$aluno['pal_foto']?>" class="img-circle" width="50%"><br>
+						<?php }?>
+
+						<label for="trocar-img"><i class="fa fa-camera" aria-hidden="true"></i>&nbsp;&nbsp;Trocar Imagem</label>
+						<input type="file" id="trocar-img" name="foto_perfil"><br>
+						<p id="arquivo-selecionado" style="color:green;">Nenhum arquivo selecionado!</p>
+						<input type="submit" disabled="disabled" id="sell-foto" class="btn btn-primary" value="Salvar">
+					</div>
+
+				</form> <!--Fim do formulario de foto-->
+				<div class="col-md-6">
+					<form method="POST" id="form-edit-projeto">
 							<input type="hidden" name="id" value="<?=$idProjeto?>">
 							<label>Nome</label>
 							<input type="text" name="nome" value="<?=utf8_encode($linhaProjeto['prj_nome'])?>" class="form-control">
@@ -28,7 +49,7 @@
                                                         <?php } else{ ?>
                                                             <input type="text" name="dt_termino" value="<?php echo date("d/m/Y", strtotime($linhaProjeto['prj_data_fim']));?>" class="form-control data">
                                                         <?php }?>
-							
+
 							<label>Tecnologias</label>
 							<input type="text" name="tecnologias" value="<?=utf8_encode($linhaProjeto['prj_tecnologias'])?>" class="form-control">
 							<div class="row">
@@ -37,10 +58,8 @@
 								</div>
 							</div>
 						</form>
-				
-			<!--</div> Fim edit projeto-->
+				</div>
+			</div>
 		</div>
-
 	</section>
-
 <?php require_once("footer.php");?>
