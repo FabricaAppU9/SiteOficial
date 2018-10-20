db2964cb16e43f16b5059b1b3dd283e95d42e6aa
maahrodrigues <maahrdg.z@gmail.com>
2018-10-20 10:50:44 -0300

    Atualizado site, manualmente

--- projetos.php
+++ projetos.php
@@ -6,11 +6,13 @@
 				Nossa equipe trabalhou com prazer e profissionalismo nos seguintes projetos. 
 			</h6>
 		</div>
-		<div class="row projects">
+		
+        <div class="row projects">
 		<div id="loader">
 		    <div class="loader-icon"></div>
 		</div>
-		<div class="col-md-12" id="portfolio-list">
+		
+        <div class="col-md-12" id="portfolio-list">
 			<ul class="cbp-rfgrid">
 	        <li style="visibility: hidden; -webkit-animation-name: none; -moz-animation-name: none; animation-name: none;-webkit-animation-duration: 1.5s; -moz-animation-duration: 1.5s; animation-duration: 1.5s;-webkit-animation-delay: 0.15s; -moz-animation-delay: 0.15s; animation-delay: 0.15s;" class="wow fadeInLeft animated" data-wow-offset="30" data-wow-duration="1.5s" data-wow-delay="0.15s">
 	        <center><a data-toggle="modal" href="#myModalProcalc" id="procalc"><img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/logoProCalc.jpg" alt="project" width="100%" style="max-width: 215px;">
@@ -42,11 +44,13 @@
                 
                 
                 
+                
+                
                 <!--teste -->
                 
                 
         <li style="visibility: hidden; -webkit-animation-name: none; -moz-animation-name: none; animation-name: none;-webkit-animation-duration: 1.5s; -moz-animation-duration: 1.5s; animation-duration: 1.5s;-webkit-animation-delay: 0.15s; -moz-animation-delay: 0.15s; animation-delay: 0.15s;" class="wow fadeInDown animated" data-wow-offset="30" data-wow-duration="1.5s" data-wow-delay="0.15s">
-	        <a data-toggle="modal" href="#myModalprojeto" id="projeto"><img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/projetos1.png" alt="project" style="border-radius: 30px;">
+	        <a data-toggle="modal" href="#myModalprojeto" id="projeto"><img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/site1.png" alt="project" style="border-radius: 30px;">
 				<!--<div class="project-info">
 					<div class="project-details">
 						<h5 class="white-text red-border-bottom"
@@ -60,14 +64,42 @@
 	        </li>         
                 
                 
+                 <li style="visibility: hidden; -webkit-animation-name: none; -moz-animation-name: none; animation-name: none;-webkit-animation-duration: 1.5s; -moz-animation-duration: 1.5s; animation-duration: 1.5s;-webkit-animation-delay: 0.15s; -moz-animation-delay: 0.15s; animation-delay: 0.15s;" class="wow fadeInDown animated" data-wow-offset="30" data-wow-duration="1.5s" data-wow-delay="0.15s">
+	        <a data-toggle="modal" href="#myModalprojeto" id="projeto"><img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/projetos1.png" alt="project" style="border-radius: 30px;">
+				<!--<div class="project-info">
+					<div class="project-details">
+						<h5 class="white-text red-border-bottom"
+						Pro-Spiro </h5>
+						<div class="details white-text">
+							Android
+						</div>
+					</div>
+				</div>-->
+				</a>
+	        </li>     
                 
+                
+                
+                 <li style="visibility: hidden; -webkit-animation-name: none; -moz-animation-name: none; animation-name: none;-webkit-animation-duration: 1.5s; -moz-animation-duration: 1.5s; animation-duration: 1.5s;-webkit-animation-delay: 0.15s; -moz-animation-delay: 0.15s; animation-delay: 0.15s;" class="wow fadeInDown animated" data-wow-offset="30" data-wow-duration="1.5s" data-wow-delay="0.15s">
+	        <a data-toggle="modal" href="#myModalprojeto" id="projeto"><img src="Zerif%20-%20Responsive%20One%20Page%20Template_arquivos/projetos1.png" alt="project" style="border-radius: 30px;">
+				<!--<div class="project-info">
+					<div class="project-details">
+						<h5 class="white-text red-border-bottom"
+						Pro-Spiro </h5>
+						<div class="details white-text">
+							Android
+						</div>
+					</div>
+				</div>-->
+				</a>
+	        </li>     
                 
                 
                 
                 
                 <!-- FIM TESTE 1 -->
 
-
+                
  		</ul>
 		</div>
 	</div>
@@ -155,6 +187,37 @@
       	<table>
       		<tr>
       			<td>Descrição</td>
+      			<td>Site Oficial da fábrica de Aplicativos.</td>
+      		</tr>
+      		<tr>
+      			<td>Tecnologias</td>
+      			<td>PHP.</td>
+      		</tr>
+      		<tr>
+      			<td></td>
+      		</tr>
+      	</table>
+      	</div>
+      	<div class="modal-footer">
+      	</div>
+      </div>
+	</div>
+</div>
+
+
+<div class="modal fade" id="myModalprojeto" role="dialog">
+	<div class="modal-dialog" style="min-width:80%;">
+		<!-- Modal content-->
+      <div class="modal-content" style="padding: 20px;min-height: auto;">
+      	<div class="modal-header" style="min-height: 40px;">
+      		
+      		<button type="button" class="close" data-dismiss="modal">&times;</button>
+      		<h3>TESTE</h3>
+      	</div>
+      	<div class="modal-body">
+      	<table>
+      		<tr>
+      			<td>Descrição</td>
       			<td>Teste.</td>
       		</tr>
       		<tr>
@@ -171,4 +234,3 @@
       </div>
 	</div>
 </div>
-
